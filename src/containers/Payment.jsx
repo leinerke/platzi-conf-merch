7baacb2e4adc0b96b12f10307a4673f31fa2513a
Leinerke Suarez <leinerke2000@gmail.com>
2022-06-17 14:14:35 -0500

    Completando la integración de pagos con la API de PayPal

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PayPalButton } from 'react-paypal-button-v2';
+import AppContext from '../context/AppContext';
 import '../styles/components/Payment.css';
 
-const Payment = () => (
-  <div className="Payment">
+const Payment = () => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+  const paypalOptions = {
+    clientId: 'ATN4MgsCVKSXinkSTL1YqlANTikW5fXyo5C7TkyVUG7JB0DTr1G2aabkWFF9Uz6kKo61tL48cfWpomc4',
+    intent: 'capture',
+    currency: 'USD',
+  };
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect',
+  };
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+      history.push('/checkout/success');
+    }
+  };
+  const handleSumTotal = () => cart.reduce((
+    accumulator,
+    currentValue,
+  ) => accumulator + currentValue.price, 0);
+
+  return (<div className="Payment">
     <div className="Payment-content">
       <h3>Resumen del pedido:</h3>
+      {cart.map(item => (
+        <div className="Payment-item" key={item.title}>
+          <div className="Payment-element">
+            <h4>{item.title}</h4>
+            <span>$ {item.price}</span>
+          </div>
+        </div>
+      ))}
       <div className="Payment-button">
-        Boton de pago con Paypal
+        <PayPalButton
+          paypalOptions={paypalOptions}
+          buttonStyles={buttonStyles}
+          amount={handleSumTotal()}
+          onPaymentStart={() => console.log('Start Payment')}
+          onPaymentSuccess={(data) => handlePaymentSuccess(data)}
+          onPaymentError={(error) => console.log(error)}
+          onPaymentCancel={(data) => console.log(data)}
+        />
       </div>
     </div>
-  </div>
-);
+  </div>);
+};
 
 export default Payment;
